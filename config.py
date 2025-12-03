7e1a45649363cecbc6697381a380034c92b5d1fd
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-12-03 21:04:30 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+
+API_ID = 123456       # Get from https://my.telegram.org
+API_HASH = "your_api_hash"
+SESSION_STRING = "user_session_string"  # Generated from Pyrogram
+DOWNLOAD_DIR = "downloads"
